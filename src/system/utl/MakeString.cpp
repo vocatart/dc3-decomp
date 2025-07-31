cea4e81beb39ec0adf1c25bd71fe8195bdddf06b
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-07-31 15:12:06 -0700

    start re-adding makestring

---
+++ src/system/utl/MakeString.cpp
@@ -0,0 +1,69 @@
+#include "utl/MakeString.h"
+#include "os/Debug.h"
+#include "os/System.h"
+
+bool bufExceeded = false;
+
+char* NextBuf(){
+    return 0;
+}
+
+FormatString::FormatString() : mBuf(NextBuf()), mBufSize(0x1000), mFmtEnd(nullptr), mType(kNone) {
+    
+}
+
+FormatString &FormatString::operator<<(int i) {
+    if (mType != kInt)
+        MILO_WARN(
+            "FormatString: '%s' doesn't start with kInt.  Format: '%s'", mFmt, mFmtBuf
+        );
+    char tmp = *mFmtEnd;
+    *mFmtEnd = '\0';
+    int n = Hx_snprintf(mBuf + 0x1000 - mBufSize, mBufSize, mFmt, i);
+    *mFmtEnd = tmp;
+    if(n < 0 && !bufExceeded){
+        bufExceeded = true;
+        MILO_WARN("MakeString() buffer size %d exceeded", 0x1000);
+    }
+    mBufSize -= n;
+    UpdateType();
+    return *this;
+}
+
+const char *FormatString::Str() {
+    if (mType != kNone)
+        MILO_WARN(
+            "FormatString: '%s' doesn't start with kNone.  Format: '%s'", mFmt, mFmtBuf
+        );
+    if (*mFmt != '\0') {
+        MILO_ASSERT(mFmtEnd - mFmt < mBufSize, 0x16F);
+        char* p2 = mFmt;
+        char* p3 = mBuf + 0x1000 - mBufSize;
+        for(; *p2 != 0; p2++, p3++){
+            *p3 = *p2;
+        }
+    }
+    return mBuf;
+}
+
+FormatString &FormatString::operator<<(const char *cc) {
+    if (mType != kStr)
+        MILO_WARN(
+            "FormatString: '%s' doesn't start with kStr.  Format: '%s'", mFmt, mFmtBuf
+        );
+    MILO_ASSERT_FMT(
+        cc < mBuf || cc >= mBuf + sizeof(mFmtBuf), "FormatString: arg in buffer"
+    );
+    char tmp = *mFmtEnd;
+    *mFmtEnd = '\0';
+    int n = Hx_snprintf(mBuf + sizeof(mFmtBuf) - mBufSize, mBufSize, mFmt, cc);
+    *mFmtEnd = tmp;
+    MILO_ASSERT(n >= 0, 0x12A);
+    mBufSize -= n;
+    UpdateType();
+    return *this;
+}
+
+FormatString::FormatString(const char* str) : mBuf(NextBuf()), mBufSize(0x1000), mFmtEnd(nullptr), mType(kNone) {
+    InitializeWithFmt(str, true);
+}
--- src/system/utl/MakeString.h
+++ src/system/utl/MakeString.h
@@ -1,10 +1,9 @@
-#ifndef UTL_MAKESTRING_H
-#define UTL_MAKESTRING_H
+#pragma once
 #include "utl/Symbol.h"
 #include "utl/Str.h"
 
 class FormatString {
-public:
+private:
     enum Type {
         kInt = 0,
         kStr = 1,
@@ -12,26 +11,19 @@ public:
         kNone = 3,
     };
 
-    char *mFmt;
-    char *mBuf;
-    int mBufSize;
-    char mFmtBuf[4096];
-    char *mFmtEnd;
-    Type mType;
+    char *mFmt; // 0x0
+    char *mBuf; // 0x4
+    int mBufSize; // 0x8
+    char mFmtBuf[0x1000]; // 0xc
+    char *mFmtEnd; // 0x100c
+    Type mType; // 0x1010
 
-    // char* mBuf;
-    // char* mNextBuf;
-    // int mBufSize;
-    // char mFmtBuf[2048];
-    // char *mFmt;
-    // Type mType;
-    // char *mFmtEnd;
-
-    FormatString();
-    FormatString(const char *);
-    void InitializeWithFmt(const char *, bool);
     void UpdateType();
-
+protected:
+    FormatString();
+    void InitializeWithFmt(const char *, bool);
+public:
+    FormatString(const char *);
     FormatString &operator<<(void *);
     FormatString &operator<<(unsigned int);
     FormatString &operator<<(unsigned long);
@@ -58,107 +50,66 @@ inline const char *MakeString(const char *c) {
 }
 
 template <class T>
-const char *MakeString(const char *c, const T &t) {
+const char *MakeString(const char *c, const T& t) {
     FormatString fs(c);
     fs << t;
     return fs.Str();
 }
 
 template <class T1, class T2>
-const char *MakeString(const char *c, const T1 &t1, const T2 &t2) {
+const char *MakeString(const char *c, const T1& t1, const T2& t2) {
     FormatString fs(c);
     fs << t1 << t2;
     return fs.Str();
 }
 
 template <class T1, class T2, class T3>
-const char *MakeString(const char *c, const T1 &t1, const T2 &t2, const T3 &t3) {
+const char *MakeString(const char *c, const T1& t1, const T2& t2, const T3& t3) {
     FormatString fs(c);
     fs << t1 << t2 << t3;
     return fs.Str();
 }
 
 template <class T1, class T2, class T3, class T4>
-const char *
-MakeString(const char *c, const T1 &t1, const T2 &t2, const T3 &t3, const T4 &t4) {
+const char *MakeString(const char *c, const T1& t1, const T2& t2, const T3& t3, const T4& t4) {
     FormatString fs(c);
     fs << t1 << t2 << t3 << t4;
     return fs.Str();
 }
 
 template <class T1, class T2, class T3, class T4, class T5>
-const char *MakeString(
-    const char *c, const T1 &t1, const T2 &t2, const T3 &t3, const T4 &t4, const T5 &t5
-) {
+const char *MakeString(const char *c, const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5) {
     FormatString fs(c);
     fs << t1 << t2 << t3 << t4 << t5;
     return fs.Str();
 }
 
 template <class T1, class T2, class T3, class T4, class T5, class T6>
-const char *MakeString(
-    const char *c,
-    const T1 &t1,
-    const T2 &t2,
-    const T3 &t3,
-    const T4 &t4,
-    const T5 &t5,
-    const T6 &t6
-) {
+const char *MakeString(const char *c, const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6) {
     FormatString fs(c);
     fs << t1 << t2 << t3 << t4 << t5 << t6;
     return fs.Str();
 }
 
 template <class T1, class T2, class T3, class T4, class T5, class T6, class T7>
-const char *MakeString(
-    const char *c,
-    const T1 &t1,
-    const T2 &t2,
-    const T3 &t3,
-    const T4 &t4,
-    const T5 &t5,
-    const T6 &t6,
-    const T7 &t7
-) {
+const char *MakeString(const char *c, const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7) {
     FormatString fs(c);
     fs << t1 << t2 << t3 << t4 << t5 << t6 << t7;
     return fs.Str();
 }
 
 template <class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8>
-const char *MakeString(
-    const char *c,
-    const T1 &t1,
-    const T2 &t2,
-    const T3 &t3,
-    const T4 &t4,
-    const T5 &t5,
-    const T6 &t6,
-    const T7 &t7,
-    const T8 &t8
-) {
+const char *
+MakeString(const char *c, const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8) {
     FormatString fs(c);
     fs << t1 << t2 << t3 << t4 << t5 << t6 << t7 << t8;
     return fs.Str();
 }
 
 template <class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9>
-const char *MakeString(
-    const char *c,
-    const T1 &t1,
-    const T2 &t2,
-    const T3 &t3,
-    const T4 &t4,
-    const T5 &t5,
-    const T6 &t6,
-    const T7 &t7,
-    const T8 &t8,
-    const T9 &t9
-) {
+const char *
+MakeString(const char *c, const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8, const T9& t9) {
     FormatString fs(c);
     fs << t1 << t2 << t3 << t4 << t5 << t6 << t7 << t8 << t9;
     return fs.Str();
 }
-
-#endif
