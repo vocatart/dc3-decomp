078eb5eba3bc72977ba830a201355f720dd064f9
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-08-06 19:40:56 -0700

    readd propsync

---
+++ src/system/obj/PropSync.cpp
@@ -0,0 +1,108 @@
+#include "obj/PropSync_p.h"
+#include "os/Debug.h"
+#include "os/File.h"
+#include "utl/FilePath.h"
+
+bool PropSync(class String &str, DataNode &node, DataArray *prop, int i, PropOp op) {
+    MILO_ASSERT(i == prop->Size() && (op & (kPropSet|kPropGet|kPropInsert)), 0x12);
+    if (op == kPropGet)
+        node = str.c_str();
+    else
+        str = node.Str();
+    return true;
+}
+
+bool PropSync(FilePath &fp, DataNode &node, DataArray *prop, int i, PropOp op) {
+    MILO_ASSERT(i == prop->Size() && (op & (kPropSet|kPropGet|kPropInsert)), 0x1C);
+    if (op == kPropGet)
+        node = FileRelativePath(FilePath::Root(), fp.c_str());
+    else {
+        const char *str = node.Str();
+        fp.Set(FilePath::Root(), str);
+    }
+    return true;
+}
+
+bool PropSync(Hmx::Color &color, DataNode &node, DataArray *prop, int i, PropOp op) {
+    MILO_ASSERT(i == prop->Size() && (op & (kPropSet|kPropGet|kPropInsert)), 0x26);
+    if (op == kPropGet)
+        node = (int)color.Pack();
+    else
+        color.Unpack(node.Int());
+    return true;
+}
+
+bool PropSync(Vector2 &vec, DataNode &node, DataArray *prop, int i, PropOp op) {
+    if (i == prop->Size())
+        return true;
+    else {
+        Symbol sym = prop->Sym(i);
+        {
+            static Symbol x("x");
+            if (sym == x) {
+                return PropSync(vec.x, node, prop, i + 1, op);
+            }
+        }
+        {
+            static Symbol y("y");
+            if (sym == y) {
+                return PropSync(vec.y, node, prop, i + 1, op);
+            }
+        }
+        return false;
+    }
+}
+
+bool PropSync(Vector3 &vec, DataNode &node, DataArray *prop, int i, PropOp op) {
+    if (i == prop->Size())
+        return true;
+    else {
+        Symbol sym = prop->Sym(i);
+        {
+            static Symbol x("x");
+            if (sym == x) {
+                return PropSync(vec.x, node, prop, i + 1, op);
+            }
+        }
+        {
+            static Symbol y("y");
+            if (sym == y) {
+                return PropSync(vec.y, node, prop, i + 1, op);
+            }
+        }
+        {
+            static Symbol z("z");
+            if (sym == z) {
+                return PropSync(vec.z, node, prop, i + 1, op);
+            }
+        }
+        return false;
+    }
+}
+
+bool PropSync(Transform &tf, DataNode &node, DataArray *prop, int i, PropOp op) {
+    if (i == prop->Size())
+        return true;
+    else {
+        Symbol sym = prop->Sym(i);
+        {
+            static Symbol x("x");
+            if (sym == x) {
+                return PropSync(tf.v.x, node, prop, i + 1, op);
+            }
+        }
+        {
+            static Symbol y("y");
+            if (sym == y) {
+                return PropSync(tf.v.y, node, prop, i + 1, op);
+            }
+        }
+        {
+            static Symbol z("z");
+            if (sym == z) {
+                return PropSync(tf.v.z, node, prop, i + 1, op);
+            }
+        }
+        return PropSync(tf.m, node, prop, i, op) != false;
+    }
+}
